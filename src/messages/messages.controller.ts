b49a3fe2d6031f4bf4900a9a13d4d143197370af
Daniel Popescu <same.dan@gmail.com>
2021-11-20 11:46:51 +0100

    Dependency Injection!

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -11,12 +11,7 @@ import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-  messagesService: MessagesService;
-
-  constructor() {
-    // DONT DO THIS IN REAL APPS
-    this.messagesService = new MessagesService();
-  }
+  constructor(public messagesService: MessagesService) {}
   @Get()
   listMessages() {
     return this.messagesService.findAll();
